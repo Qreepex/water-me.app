90107fc104cbb60a26c0ffdca339d6e75a37eb24
Ben Schiemann <dev@qreepex.xyz>
2026-01-25 13:56:25 +0100

    fix svelte rerender

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -11,7 +11,10 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({ pages: 'www', assets: 'www', fallback: null, strict: false })
+		adapter: adapter({ pages: 'www', assets: 'www', fallback: '404.html', strict: false }),
+		prerender: {
+			handleMissingId: 'ignore'
+		}
 	}
 };
 
