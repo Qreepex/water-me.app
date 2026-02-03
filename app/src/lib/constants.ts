a05a2dd78c705e7717c984bd7d0c97565f6fe5e9
Ben Schiemann <dev@qreepex.xyz>
2026-02-03 22:10:32 +0100

    new repo url

--- app/src/lib/constants.ts
+++ app/src/lib/constants.ts
@@ -4,4 +4,4 @@ export const WEBSITE_URL = 'https://water-me.app';
 export const PRIVACY_POLICY_URL = 'https://water-me.app/privacy';
 export const TERMS_OF_SERVICE_URL = 'https://water-me.app/terms';
 export const IMPRINT_URL = 'https://water-me.app/imprint';
-export const SOURCE_CODE_URL = 'https://github.com/qreepex/plants';
+export const SOURCE_CODE_URL = 'https://github.com/qreepex/water-me-app';
